1b2183290593154a4b3d7e13c909226617ac3664
tanfly <mcewen.stella@gmail.com>
2019-12-01 19:35:56 -0600

    added _url to database url

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_MAROON'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_MAROON_URL'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
